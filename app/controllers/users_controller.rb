1091cbf632423988f891f22e89730cfb6016f6b8
Nina Angelo <ninacelie@gmail.com>
2020-03-26 23:37:52 -0500

    creates users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController 
 
   def new
+    @user = User.new 
   end 
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      session[:user_id] = @user.id # create a session / log them in 
+      redirect_to "/users/#{@user.id}"
+    else
+      render :new
+    end 
   end 
   
   def show
   end 
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end 
