7e3bf5616b598e159eed7d3138d11a4b666914a2
Nina Angelo <ninacelie@gmail.com>
2020-03-26 21:51:36 -0500

    creates sign up routes and forms

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :items, through: :users 
 
-  validates :title, presence: true
-  validates :content, presence: true 
+  # validates :title, presence: true
+  # validates :content, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password # gives access to built in validations and using bcrypt 
 
   has_many :lists
   has_many :items, through: :lists 
 
-  validates :username, presence: true 
-  validates :email, presence: true 
+  # validates :username, presence: true 
+  # validates :email, presence: true 
 end
